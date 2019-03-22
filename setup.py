a3af86558cc7734b706620067aa4bac50dc3f932
Ben <bstriner@gmail.com>
2019-03-22 18:56:35 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='graph-lm',
+      version='0.0.1',
+      #install_requires=['tensorflow'],
+      author='Ben Striner',
+      url='https://github.com/bstriner/graph-lm',
+      packages=find_packages())
